417ea056213a1be4b99186a7317863a2eb055f64
2130649 <2130649@SB0134-59510>
2023-11-16 14:42:37 -0500

    Ajoutez des fichiers projet.

---
+++ Projet_Final/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Devices.Enumeration;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace Projet_Final
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+
+        private void navView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+            var item = (NavigationViewItem)args.SelectedItem;
+
+            /*
+            switch (item.Name)
+            {
+                case "iClients":
+                    mainFrame.Navigate(typeof(Page1));
+                    break;
+                case "iAgenda":
+                    mainFrame.Navigate(typeof(Page2));
+                    break;
+                case "iAgenda2":
+                    mainFrame.Navigate(typeof(Page4));
+                    break;
+                default:
+                    break;
+            }
+            */
+
+        }
+    }
+}
