e28723fa3c6fb7ba5853757ac4a7f2a3fb83c901
2130649 <2130649@SB0134-59520>
2023-11-20 11:01:37 -0500

    Class Client

---
+++ Projet_Final/Client.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet_Final
+{
+    class Client
+    {
+        private int id;
+        private string nom;
+        private string adresse;
+        private string num;
+        private string email;
+
+        public Client(int id, string nom, string adresse, string num, string email)
+        {
+            this.id = id;
+            this.nom = nom;
+            this.adresse = adresse;
+            this.num = num;
+            this.email = email;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Nom { get => nom; set => nom = value; }
+        public string Adresse { get => adresse; set => adresse = value; }
+        public string Num { get => num; set => num = value; }
+        public string Email { get => email; set => email = value; }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Client client &&
+                   id == client.id &&
+                   nom == client.nom &&
+                   adresse == client.adresse &&
+                   num == client.num &&
+                   email == client.email;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(id, nom, adresse, num, email);
+        }
+
+        public override string ToString()
+        {
+            return $"Id: {Id} Nom: {Nom}";
+        }
+    }
+}
