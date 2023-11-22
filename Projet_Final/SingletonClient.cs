1b14223656b8d06211f89ecd2a3a3d3da429a3b1
Caporal6 <xavier.2@hotmail.ca>
2023-11-22 18:42:46 -0500

    Ajout de Client

---
+++ Projet_Final/SingletonClient.cs
@@ -0,0 +1,74 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet_Final
+{
+    class SingletonClient
+    {
+        ObservableCollection<Client> liste;
+        static SingletonClient instance = null;
+
+        MySqlConnection con = new MySqlConnection("Server=adresseServeur;Database=nomdeBD;Uid=nomUtilisateur;Pwd=motdepassse;");
+
+        public SingletonClient()
+        {
+            liste = new ObservableCollection<Client>();
+        }
+
+        public static SingletonClient getInstance()
+        {
+            if (instance == null)
+                instance = new SingletonClient();
+
+            return instance;
+        }
+
+        public void ajouter_Client(string nom,string adresse, string num, string email)
+        {
+            try
+            {
+                MySqlCommand commande = new MySqlCommand();
+                commande.Connection = con;
+                commande.CommandText = "insert into clients values(@nom, @adresse, @num, @email) ";
+
+                commande.Parameters.AddWithValue("@nom", nom);
+                commande.Parameters.AddWithValue("@adresse", adresse);
+                commande.Parameters.AddWithValue("@num", num);
+                commande.Parameters.AddWithValue("@email", email);
+
+                con.Open();
+                commande.Prepare();
+                commande.ExecuteNonQuery();
+
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                //message d'erreur
+            }
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
